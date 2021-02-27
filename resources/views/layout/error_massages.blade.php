acfe07be86768285a33987ce02bde235e2571cf5
masayuki <masayuki201_201@yahoo.co.jp>
2021-02-28 08:57:13 +0900

    update:error_messages.blade.php 完成#21

--- resources/views/layout/error_massages.blade.php
+++ resources/views/layout/error_massages.blade.php
@@ -9,8 +9,6 @@
 {{-- フラッシュメッセージ --}}
 @if (session('flash_message'))
     <div class="flash_message alert alert-danger text-center py-3 my-0">
-
         {{ session('flash_message') }}
     </div>
-
 @endif
