9d3114aed16a50ce1544623b3d9ebb097332f619
masayuki <masayuki201_201@yahoo.co.jp>
2021-02-28 09:41:54 +0900

    update:top.blade.php ポタン追加#23

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -5,24 +5,48 @@
 @section('title','ちるび/topページ')
 
 @section('content')
-<div class="container">
-    <h1 class="text-center py-5">@yield('title')</h1>
 
+<nav class="navbar navbar-expand-sm navbar-dark" style="background-color: rgb(30, 141, 225)">
+    <div class="collapse navbar-collapse">
+    <ul class="navbar-nav">
+        <li class="nav-item mx-2">
+            <a class="nav-link text-light" href="#">ピックアアップ</a>
+        </li>
+        <li class="nav-item mx-4">
+            <a class="nav-link text-light" href="#">ランキング</a>
+        </li>
+        <li class="nav-item mx-4">
+            <a class="nav-link text-light" href="#">みんなの動画</a>
+        </li>
+        <li class="nav-item mx-4">
+            <a class="nav-link text-light" href="#">ヘルプ</a>
+        </li>
+    </ul>
+    </div>
+</nav>
+
+<div class="container">
     {{-- ログインしていない場合 --}}
     @guest
     <div class="row">
-        <div class="col-xs-12 col-md-6 text-center mt-5">
-            <h5>まだアカウントを<br>お持ちでない方はこちら</h5>
-            <div class="col-xs-12 col-md-12 text-center mt-4">
-            </div>
+        <div class="col-xs-12 col-md-6 text-center my-5">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="btn btn-danger col-auto" href="#">新規登録</a>
+                </li>
+            </ul>
         </div>
-        <div class="col-xs-12 col-md-6 text-center mt-5">
-            <h5>すでにアカウントを<br>お持ちの方はこちら</h5>
-            <div class="col-xs-12 col-md-12 text-center mt-4">
-            </div>
+
+        <div class="col-xs-12 col-md-6 text-center my-5">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="btn btn-primary col-auto" href="#">ログイン</a>
+                </li>
+            </ul>
         </div>
     </div>
     @endguest
+
 
     {{-- ログインしている場合 --}}
     @auth
