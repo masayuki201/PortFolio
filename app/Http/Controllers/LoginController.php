d17c1eb0ad352148c533812bb1a228e61a298f5d
masayuki <masayuki201_201@yahoo.co.jp>
2021-02-28 15:53:02 +0900

    update:Login.blade.php ボタン配置修正#30

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -42,6 +42,4 @@ class LoginController extends Controller
         return view('login');
     }
 
-
-
 }
--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -29,9 +29,6 @@ class RegisterController extends Controller
         ]);
     }
 
-
-
-
     protected function create(array $data)
     {
         return User::create([
