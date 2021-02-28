60adc60f672c9cf8b1ce472782e77cf0dded01dd
masayuki <masayuki201_201@yahoo.co.jp>
2021-02-28 13:30:13 +0900

    add:register.blade.php内容追加#27

---
+++ resources/views/register.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="text-center">
+        <h3 class="login_title text-left d-inline-block mt-5">新規登録</h3>
+    </div>
+
+    <div class="row mt-5 mb-5">
+        <div class="col-sm-6 offset-sm-3">
+
+            {!! Form::open(['route' => 'signup.post']) !!}
+                <div class="form-group">
+                    {!! Form::label('nickname', 'ニックネーム') !!}
+                    {!! Form::text('nickname', old('nickname'), ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('email', 'メールアドレス') !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('password', 'パスワード') !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('password_confirmation', 'パスワード確認') !!}
+                    {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+                </div>
+
+                {!! Form::submit('新規登録', ['class' => 'btn btn-primary mt-2']) !!}
+            {!! Form::close() !!}
+
+        </div>
+    </div>
+
+@endsection
