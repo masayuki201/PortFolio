19ed8f0e301886d6d6503b8ae595a557b2113873
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-01 17:45:26 +0900

    add:UserController.php内容追加#34

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+use Illuminate\Validation\Rule;
+
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('top');
+    }
+
+    //ユーザ情報照会
+    public function show($id)
+    {
+        if($id == Auth::id()){
+            return view('detail',[ 'id' => $id ]);
+        }
+       // return redirect('/   ')->with('flash_message', '不適切なURLです。');
+
+    }
+
 }
