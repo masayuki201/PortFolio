2ddb8cf0019ebd4a06929989ba027b2e77173f71
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-04 03:16:04 +0900

    add:VideosTableSeeder.php内容追加#45

--- database/seeds/VideosTableSeeder.php
+++ database/seeds/VideosTableSeeder.php
@@ -11,6 +11,37 @@ class VideosTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('videos')->insert([
+            [
+                'id' => '1',
+                'user_id' =>'1',
+                'url' => 'PkDfrVdCwCs',
+                'target_id' => '0,1',
+            ],
+            [
+                'id' => '2',
+                'user_id' =>'2',
+                'url' => 'NCJZgcvh_Xs',
+                'target_id' => '3',
+            ],
+            [
+                'id' => '3',
+                'user_id' =>'3',
+                'url' => '5NHinUvC2wg',
+                'target_id' => '3',
+            ],
+            [
+                'id' => '4',
+                'user_id' =>'4',
+                'url' => 'HHO22TK78Yo',
+                'target_id' => '2,3',
+            ],
+            [
+                'id' => '5',
+                'user_id' =>'5',
+                'url' => 'ZioFWxb1U0s',
+                'target_id' => '2',
+            ],
+        ]);
     }
 }
