2703ebcfe9d8f40fa79abecf2fb9b90aa16b64ef
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-06 05:39:03 +0900

    update:CategoriesTableSeeder.php insert内容修正#47

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -13,11 +13,21 @@ class CategoriesTableSeeder extends Seeder
     {
         DB::table('categories')->insert([
             [
+                'id' => '0'
+                'target_grade' => '年少々',
+            ],
+            [
                 'id' => '1'
-                'target_grade' => ''
-
-
-            ]
-        ])
+                'target_grade' => '年少',
+            ],
+            [
+                'id' => '2'
+                'target_grade' => '年中',
+            ],
+            [
+                'id' => '3'
+                'target_grade' => '年長 ',
+            ],
+        ]);
     }
 }
