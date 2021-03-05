e4eaf294c111bb084b23745d60773cc62518100c
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-05 22:18:19 +0900

    add:CategoriesTableSeeder.php 内容追加#47

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -11,6 +11,13 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('categories')->insert([
+            [
+                'id' => '1'
+                'target_grade' => ''
+
+
+            ]
+        ])
     }
 }
