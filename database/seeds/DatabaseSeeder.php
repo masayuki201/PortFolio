096657031464d940beefbcb827a3087a6f1bdca9
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-07 15:48:31 +0900

    update:DatabaseSeeder コンフリクト修正#50

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,11 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
-
-=======
-        $this->call(TargetsTableSeeder::class);
->>>>>>> develop
         $this->call(UsersTableSeeder::class);
         $this->call(TargetsTableSeeder::class);
         $this->call(VideosTableSeeder::class);
