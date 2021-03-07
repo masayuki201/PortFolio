385d14bf136fc52c4a4ee8ca2e19644bd2ad209e
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-07 10:45:58 +0900

    add:Video.php 内容追加#51

--- app/Video.php
+++ app/Video.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Video extends Model
 {
-    //
+    protected $fillable = ['user_id','url','target_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
