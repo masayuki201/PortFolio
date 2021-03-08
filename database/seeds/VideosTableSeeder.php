99df6da1a1ae7ecba7125ccfc3ce5b144349bdbc
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-09 04:44:11 +0900

    fix:VideosTableSeeder.php 修正#56

--- database/seeds/VideosTableSeeder.php
+++ database/seeds/VideosTableSeeder.php
@@ -15,35 +15,30 @@ class VideosTableSeeder extends Seeder
     {
         DB::table('videos')->insert([
             [
-                'id' => '1',
                 'user_id' =>'1',
                 'url' => 'PkDfrVdCwCs',
                 'target_id' => '1',
                 'regist_date' => date('Y-m-d H:i:s'),
             ],
             [
-                'id' => '2',
                 'user_id' =>'2',
                 'url' => 'NCJZgcvh_Xs',
                 'target_id' => '3',
                 'regist_date' => date('Y-m-d H:i:s'),
             ],
             [
-                'id' => '3',
                 'user_id' =>'3',
                 'url' => '5NHinUvC2wg',
                 'target_id' => '3',
                 'regist_date' => date('Y-m-d H:i:s'),
             ],
             [
-                'id' => '4',
                 'user_id' =>'4',
                 'url' => 'HHO22TK78Yo',
                 'target_id' => '2',
                 'regist_date' => date('Y-m-d H:i:s'),
             ],
             [
-                'id' => '5',
                 'user_id' =>'5',
                 'url' => 'ZioFWxb1U0s',
                 'target_id' => '1',
