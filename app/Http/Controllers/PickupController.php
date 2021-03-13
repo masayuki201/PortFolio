098d63a4d9bf0c959f3ea055fdaf84470f3b839e
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-13 13:39:06 +0900

    add:PickupController.php 内容追加#72

--- app/Http/Controllers/PickupController.php
+++ app/Http/Controllers/PickupController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Video;
 
 class PickupController extends Controller
 {
-    //
+    public function index()
+    {
+        DB::table('videos')->inRandomOrder()->take(4)->get();
+
+
+        return view('pickup');
+    }
+
 }
