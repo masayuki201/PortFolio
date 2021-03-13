6f12987fcb93ae8d2e37918842fdfe1fb924563c
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-13 10:45:58 +0900

    fix:Video.php timestamps修正#70

--- app/Http/Controllers/VideosController.php
+++ app/Http/Controllers/VideosController.php
@@ -10,10 +10,11 @@ use Illuminate\Support\Facades\Auth;
 
 class VideosController extends Controller
 {
+    //動画登録ページ
     public function create()
     {
         $user = Auth::user();
-        $videos = $user->videos->orderBy('id', 'desc')->paginate(9);
+        $videos = $user->videos()->orderBy('id', 'desc')->paginate(9);
 
         $data=[
             'user' => $user,
@@ -22,21 +23,25 @@ class VideosController extends Controller
         return view('create', $data);
     }
 
+
     //動画登録フォームからの動画登録アクション
     public function store(Request $request)
     {
-
         $this->validate($request,[
             'url' => 'required|max:11',
             'target_id' => 'max:10',
         ]);
 
+
+
         $request->user()->videos()->create([
             'url' => $request->url,
             'target_id' => $request->target_id,
         ]);
+
         return back();
     }
+
 
     //動画削除アクション
     public function destroy($id)
