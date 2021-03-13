facc970f79537dfe3302f526b7ee09124bd6f899
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-14 06:19:37 +0900

    add:ranking.blade.php ファイル追加#37

--- app/Pickup.php
+++ app/Pickup.php
@@ -6,18 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pickup extends Model
 {
-
-    protected $fillable = [
-        'id',
-        'nickname',
-        'email',
-        'password',
-    ];
-
-    public function video()
-    {
-        return $this->belongsTo(Video::class);
-    }
-
-
+ //
 }
