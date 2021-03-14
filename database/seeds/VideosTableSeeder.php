d0a0e1aa300bc9996a716096523ac8ba8b7afa76
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-14 14:55:47 +0900

    add:VideosTableSeeder.php 動画追加#79

--- database/seeds/VideosTableSeeder.php
+++ database/seeds/VideosTableSeeder.php
@@ -44,6 +44,96 @@ class VideosTableSeeder extends Seeder
                 'target_id' => '1',
                 'regist_date' => date('Y-m-d H:i:s'),
             ],
+            [
+                'user_id' =>'6',
+                'url' => 'fBrJVgq-DnY',
+                'target_id' => '3',
+                'regist_date' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' =>'7',
+                'url' => 'FGOjv96y53M',
+                'target_id' => '4',
+                'regist_date' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' =>'8',
+                'url' => 'nSQnMV45rJs',
+                'target_id' => '2',
+                'regist_date' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' =>'1',
+                'url' => '5Q2Nzn2O-xk',
+                'target_id' => '1',
+                'regist_date' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' =>'2',
+                'url' => 'MLplrBWOmOk',
+                'target_id' => '3',
+                'regist_date' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' =>'3',
+                'url' => 'lAPNiQB1Z7s',
+                'target_id' => '4',
+                'regist_date' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' =>'4',
+                'url' => 'KaVS4yQUK6o',
+                'target_id' => '2',
+                'regist_date' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' =>'5',
+                'url' => 'gOwhKcutnpc',
+                'target_id' => '1',
+                'regist_date' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' =>'6',
+                'url' => 'Oj62PMMT4Bk',
+                'target_id' => '2',
+                'regist_date' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' =>'7',
+                'url' => 'cPt926Cx_fI',
+                'target_id' => '4',
+                'regist_date' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' =>'8',
+                'url' => 'vHhiPKeYe60',
+                'target_id' => '1',
+                'regist_date' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' =>'1',
+                'url' => 'gF4m7sCQ-4c',
+                'target_id' => '2',
+                'regist_date' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' =>'2',
+                'url' => 'VvY9TEDtets',
+                'target_id' => '3',
+                'regist_date' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' =>'3',
+                'url' => 'VyKLQXOj0ts',
+                'target_id' => '4',
+                'regist_date' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' =>'4',
+                'url' => '3aCOg-ZnCmI',
+                'target_id' => '4',
+                'regist_date' => date('Y-m-d H:i:s'),
+            ],
         ]);
     }
 }
