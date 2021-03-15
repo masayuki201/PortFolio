b3d967939818856e1e7304757acde50c1dfadfca
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-16 04:21:37 +0900

    add:Target.php 内容追加#92

--- app/Target.php
+++ app/Target.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Target extends Model
 {
+    protected $fillable = ['target_grade',];
+
+    public function videos()
+    {
+        return $this->hasMany(Video::class);
+    }
 
 }
