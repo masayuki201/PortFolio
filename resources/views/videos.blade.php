99d7717d5bab90fd08dbbd7a951775fb3d2c80f9
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-18 03:00:23 +0900

    update:videos.balde.php ニックネーム・対象年齢表示修正#98

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -3,34 +3,39 @@
         <h4>登録済み動画</h4>
     </div>
     <div class="video row mt-5 text-center">
-        @foreach ($videos as $key => $video)
-        @if($loop->iteration % 3 == 1 && $loop->iteration != 1)
-    </div>
-    <div class="row text-center mt-3">
-        @endif
-        <div class="col-lg-4 mb-5">
-            <div class="video text-left d-inline-block">
-                <div>
-                    @if($video)
-                    <iframe width="290" height="163.125" src="{{ 'https://www.youtube.com/embed/'.$video->url }}?controls=1&loop=1&playlist={{ $video->url }}" frameborder="0"></iframe>
-                    @else
-                    <iframe width="290" height="163.125" src="https://www.youtube.com/embed/" frameborder="0"></iframe>
-                    @endif
+        @foreach ($videos as $key => $user)
+
+        @php
+
+        $video=$user;
+
+        @endphp
+            @if($loop->iteration % 3 == 1 && $loop->iteration != 1)
                 </div>
-                <p>
-                    @if(isset($video->target_id))
-                    {{ $video->target_id }}
-                    @endif
-                </p>
+                <div class="row text-center mt-3">
+            @endif
+                <div class="col-lg-4 mb-5">
+                    <div class="video text-left d-inline-block">
+                        ＠{{ $video->user->nickname }}
+                        <div>
+                            @if($video)
+                                <iframe width="290" height="163.125" src="{{ 'https://www.youtube.com/embed/'.$video->url }}?controls=1&loop=1&playlist={{ $video->url }}" frameborder="0"></iframe>
+                            @else
+                                <iframe width="290" height="163.125" src="https://www.youtube.com/embed/" frameborder="0"></iframe>
+                            @endif
+                        </div>
+                        <p>
+                        おすすめ：{{ $video->target['target_grade'] }}さん
+                        </p>
 
-                @if(Auth::id() == $video->user_id)
-                {!! Form::open(['route' => ['videos.destroy', $video->id], 'method' => 'delete']) !!}
-                    {!! Form::submit('削除', ['class' => 'button btn btn-danger']) !!}
-                {!! Form::close() !!}
-                @endif
+                        @if(Auth::id() == $video->user_id)
+                        {!! Form::open(['route' => ['videos.destroy', $video->id], 'method' => 'delete']) !!}
+                        {!! Form::submit('削除', ['class' => 'button btn btn-danger']) !!}
+                        {!! Form::close() !!}
+                        @endif
 
-            </div>
-        </div>
+                    </div>
+                </div>
         @endforeach
     </div>
 </div>
