3534557372aef794557dc805b49dcd77f86d0c25
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-26 16:20:15 +0900

    update:app.blade.php stylesheet 修正#115

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -6,12 +6,8 @@
 
 @section('content')
 
-　　{{-- 背景画を入れる--}}
-
-
+{{-- "junbotron" 背景画を入れる--}}
 <div class="jumbotron">
-    {{-- <img src="/images/s.jpg"> --}}
-
     <div class="container">
         <div class="text-left text-light">
             <h2>さぁ みんなで作っていこう<br>子供向けの動画を共有しよう</h2>
