6440c5d100691b60c533bd68c1127f11e1aff13d
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-26 17:23:33 +0900

    add:style css コメント追加#115

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -10,7 +10,7 @@
 <div class="jumbotron jumbotron-extend">
     <div class="container-fluid jumbotron-container">
         <div class="text-left text-light">
-            <h2>さぁ みんなで作っていこう<br>子供向けの動画を共有しよう</h2>
+            <h1>さぁ みんなで作っていこう<br>子供向けの動画を共有しよう</h1>
         </div>
     </div>
 </div>
