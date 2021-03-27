c943c7ae57fef2734e2df11125bc5dd1a3280c60
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-28 06:46:56 +0900

    fix:footer.blade.php ログイン時のlink先修正#127

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -4,7 +4,8 @@
         <a class="navbar-brand text-dark" href="/"><h6>Children's<br>Videos</h6></a>
         <a class="navbar-brand text-dark" href="/"><h3>ちるび</h3></a>
 
-        {{-- 右寄せメニュー --}}
+        {{-- 右寄せメニュー ログインしていない時 --}}
+        @guest
         <div class="collapse navbar-collapse flex-column" >
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item mx-2">
@@ -21,5 +22,27 @@
                 </li>
             </ul>
         </div>
+        @endguest
+
+        {{-- 右寄せメニュー ログインしている時--}}
+        @auth
+        <div class="collapse navbar-collapse flex-column" >
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item mx-2">
+                    <a class="nav-link text-dark" href="/pickup">ピックアップ</a>
+                </li>
+                <li class="nav-item mx-4">
+                    <a class="nav-link text-dark" href="ranking">ランキング</a>
+                </li>
+                <li class="nav-item mx-4">
+                    <a class="nav-link text-dark" href="/users">みんなの動画</a>
+                </li>
+                <li class="nav-item mx-4">
+                    <a class="nav-link text-dark" href="/videos/create">動画登録</a>
+                </li>
+            </ul>
+        </div>
+        @endauth
     </nav>
+
 </footer>
