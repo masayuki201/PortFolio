19dc411afea215e13cd31a3cae5afaa59f56f86d
masayuki <masayuki201_201@yahoo.co.jp>
2021-03-28 07:33:12 +0900

    update:create.blade.php 文字表記の修正#131

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -11,12 +11,12 @@
     <div class="video row">
         {!! Form::open(['route'=>'videos.store']) !!}
         <div class="form-group">
-            {!! Form::label('url','①登録したいYouTube動画のURLを入力して下さい',['class'=>'text-success']) !!}
+            {!! Form::label('url','①登録したいYouTube動画のURLを入力してね♪',['class'=>'text-success']) !!}
             <br>例）登録したいYouTube動画のURLが <span> https://www.youtube.com/watch?v=PkDfrVdCwCs なら</span>
-            <div>  "v="の直後にある "<span class="text-success">”PkDfrVdCwCs”</span>" を入力</div>
+            <div>  "v="の直後にある "<span class="text-success">”PkDfrVdCwCs”</span>" を入力してね♪</div>
             {!! Form::text('url',null,['class'=>'form-control']) !!}
 
-            {!! Form::label('comment','②登録する動画のオススメの対象を選択して下さい（1対象のみ）',['class'=> 'text-success mt-5']) !!}
+            {!! Form::label('comment','②登録する動画のおすすめの対象を選択してね♪（※1対象のみ）',['class'=> 'text-success mt-5']) !!}
             <br>
             {{-- チェックボックス　--}}
             <div class="form-check-inline">
