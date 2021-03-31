1079b629ff1d369a02d49bc43c3a89eaab2241bb
masayuki <masayuki201_201@yahoo.co.jp>
2021-04-01 08:03:32 +0900

    add:pickup.blade.php コメントの追加#147

--- resources/views/pickup.blade.php
+++ resources/views/pickup.blade.php
@@ -12,6 +12,7 @@
         @php
             $video=$user;
         @endphp
+        {{-- 動画を横に3つずつ表示させる --}}
             @if($loop->iteration % 3 == 1 && $loop->iteration != 1)
                 </div>
                 <div class="row text-center mt-3">
