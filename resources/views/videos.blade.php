a3373cec310471d80346d50e975f85882cba6d9f
masayuki <masayuki201_201@yahoo.co.jp>
2021-04-07 03:29:11 +0900

    add:videos.blade.php コメント追加＃163

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -10,14 +10,17 @@
         $video=$user;
 
         @endphp
+            {{-- 動画を横に3つずつ表示させる --}}
             @if($loop->iteration % 3 == 1 && $loop->iteration != 1)
                 </div>
                 <div class="row text-center mt-3">
             @endif
                 <div class="col-lg-4 mb-5">
                     <div class="video text-left d-inline-block">
+                        {{-- ニックネーム表示 --}}
                         ＠{{ $video->user->nickname }}
                         <div>
+                            {{-- 動画を表示 --}}
                             @if($video)
                                 <iframe width="290" height="163.125" src="{{ 'https://www.youtube.com/embed/'.$video->url }}?controls=1&loop=1&playlist={{ $video->url }}" frameborder="0"></iframe>
                             @else
@@ -25,6 +28,7 @@
                             @endif
                         </div>
                         <p>
+                        {{-- 対象学年表示 --}}
                         おすすめ：{{ $video->target['target_grade'] }}さん
                         </p>
 
