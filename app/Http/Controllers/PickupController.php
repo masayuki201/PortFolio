04f1684213859e9eb271105811a14db1e8ceba5e
masayuki <masayuki201_201@yahoo.co.jp>
2021-04-09 22:59:50 +0900

    add:PickupController.php コメント追加#168

--- app/Http/Controllers/PickupController.php
+++ app/Http/Controllers/PickupController.php
@@ -13,8 +13,10 @@ class PickupController extends Controller
     //ピックアップ
     public function index()
     {
+        // DBよりランダムに9個のビデオを選定し、表示する
         $pickup = Video::inRandomOrder()->take(9)->get();
 
+        // pickup.blade.phpを表示させる
         return view('pickup',['pickup' => $pickup,]);
     }
 
