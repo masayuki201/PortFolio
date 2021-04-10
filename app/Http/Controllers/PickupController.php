9008900a8e73e5d46ede23a6c50643b60d48968a
masayuki <masayuki201_201@yahoo.co.jp>
2021-04-10 10:02:28 +0900

    update:PickupController.php インデント修正#165

--- app/Http/Controllers/PickupController.php
+++ app/Http/Controllers/PickupController.php
@@ -2,21 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\User;
 use App\Video;
-use App\Pickup;
 
 class PickupController extends Controller
 {
     //ピックアップ
     public function index()
     {
+        // DBよりランダムに9個のビデオを選定し、表示する
         $pickup = Video::inRandomOrder()->take(9)->get();
 
+        // pickup.blade.phpを表示させる
         return view('pickup',['pickup' => $pickup,]);
     }
-
-
 }
