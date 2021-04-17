20571f8ea1359cd4ecb8a50dced0f055eae669f1
masayuki <masayuki201_201@yahoo.co.jp>
2021-04-17 14:21:00 +0900

    add:Target.php コメント追加#188

--- app/Target.php
+++ app/Target.php
@@ -8,6 +8,7 @@ class Target extends Model
 {
     protected $fillable = ['target_grade'];
 
+    //TargetモデルがVideoモデルを所有している
     public function videos()
     {
         return $this->hasMany(Video::class);
