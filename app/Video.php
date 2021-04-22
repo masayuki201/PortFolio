d76d4048a3623864b0aaeb747611ba8a3594ff09
masayuki <masayuki201_201@yahoo.co.jp>
2021-04-23 03:32:38 +0900

    update:Video.php コメント修正#204

--- app/Video.php
+++ app/Video.php
@@ -9,15 +9,15 @@ class Video extends Model
      // videos_tableの内容設定
     protected $fillable = ['user_id','url','target_id',];
 
+    // Videoモデルが、Userモデルに所属している
     public function user()
     {
-        // Videoモデルが、Userモデルに所属している
         return $this->belongsTo(User::class);
     }
 
+    // Videoモデルが、Targetモデルに所属している
     public function target()
     {
-        // Videoモデルが、Targetモデルに所属している
         return $this->belongsTo(Target::class);
     }
 
