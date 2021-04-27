34f6a2ce4667b80cba79f9e642e92dcd9fc0f5a3
masayuki <masayuki201_201@yahoo.co.jp>
2021-04-28 03:29:44 +0900

    update:footer.blade.php  footer文字配色修正#218

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-sm navbar-dark" style="background-color: rgb(245, 138, 7)">
         {{-- サイトロゴ --}}
         <a class="navbar-brand text-light" href="/"><h6>Children's<br>Videos</h6></a>
-        <a class="navbar-brand text-dark" href="/"><h3>ちるび</h3></a>
+        <a class="navbar-brand text-light" href="/"><h3>ちるび</h3></a>
 
         {{-- 右寄せメニュー ログインしていない時 --}}
         @guest
