e8923dc233e91f5007b4d7d92118cfe3db32ec7b
masayuki <masayuki201_201@yahoo.co.jp>
2021-05-01 17:31:54 +0900

    add:app.blade.php Font AwesomeのCDNを追加#242

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -11,6 +11,9 @@
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/style.css">
 
+    {{-- Font Awesome CDN読み込み --}}
+    <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet">
+
     <title>@yield('title')</title>
 </head>
 
