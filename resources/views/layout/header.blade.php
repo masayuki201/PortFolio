ae8347ca989f43fcf62b3ca3a1c93e87b3265a5a
masayuki <masayuki201_201@yahoo.co.jp>
2021-05-02 05:50:46 +0900

    add:header.blade.php ログアウト時ランキングにアイコン追加#250

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -36,7 +36,10 @@
                 </a>
             </li>
             <li class="nav-item mx-4">
-                <a class="nav-link text-light" href="/ranking">ランキング</a>
+                <a class="nav-link text-light faa-parent  animated-hover" href="/ranking">
+                    <i class="fa fa-flag-checkered faa-shake "></i>
+                    ランキング
+                </a>
             </li>
             <li class="nav-item mx-4">
                 <a class="nav-link text-light" href="/users">みんなの動画</a>
