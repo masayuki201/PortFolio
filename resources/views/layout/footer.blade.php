5e76e1082ad2e06605beb22694da357ec76a1a43
masayuki <masayuki201_201@yahoo.co.jp>
2021-05-02 08:30:43 +0900

    add:footer.blade.php ログアウト時動画登録にアイコン追加#260

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -27,7 +27,10 @@
                     </a>
                 </li>
                 <li class="nav-item mx-4">
-                    <a class="nav-link text-light" href="/login">動画登録</a>
+                    <a class="nav-link text-light faa-parent  animated-hover" href="/login">
+                        <i class="fa fa-caret-square-right faa-bounce"></i>
+                        動画登録
+                    </a>
                 </li>
             </ul>
         </div>
