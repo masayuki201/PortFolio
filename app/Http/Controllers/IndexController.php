ff0d4e4d8678ac80a4721a72573143cba265dca0
masayuki <masayuki201_201@yahoo.co.jp>
2021-05-07 04:39:17 +0900

    update:IndexController.php functuon index修正#87

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
+    //みんなの動画表示
+    public function index()
+    {
+        $users = Video::all();
+    // Users.blade.phpを表示させる
+        return view('users', ['users' => $users,]);
+    }
 }
