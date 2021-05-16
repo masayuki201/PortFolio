2fff0e8997ca1b8479410c6c42c3c1c5605297d5
masayuki <masayuki201_201@yahoo.co.jp>
2021-05-16 14:08:59 +0900

    add:UsersTableSeeder.php ユーザIDコメント追加#344

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,48 +14,56 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->insert([
             [
+                // ユーザID:1
                 'nickname' => '信長のパパ',
                 'email'=> 'oda@oda.com',
                 'password' => bcrypt('password'),
                 'delete_flag' => '1',
             ],
             [
+                // ユーザID:2
                 'nickname' => '秀吉のママ',
                 'email'=> 'toyo@otoyo.com',
                 'password' => bcrypt('password'),
                 'delete_flag' => '1',
             ],
             [
+                // ユーザID:3
                 'nickname' => '式部のママ',
                 'email'=> 'mura@omura.com',
                 'password' => bcrypt('password'),
                 'delete_flag' => '1',
             ],
             [
+                // ユーザID:4
                 'nickname' => '納言のパパ',
                 'email'=> 'sei@sei.com',
                 'password' => bcrypt('password'),
                 'delete_flag' => '1',
             ],
             [
+                // ユーザID:5
                 'nickname' => '家康のパパ',
                 'email'=> 'toku@toku.com',
                 'password' => bcrypt('password'),
                 'delete_flag' => '1',
             ],
             [
+                // ユーザID:6
                 'nickname' => '光秀のじいじ',
                 'email'=> 'ake@ake.com',
                 'password' => bcrypt('password'),
                 'delete_flag' => '1',
             ],
             [
+                // ユーザID:7
                 'nickname' => '義経のばあば',
                 'email'=> 'ashi@ashi.com',
                 'password' => bcrypt('password'),
                 'delete_flag' => '1',
             ],
             [
+                // ユーザID:8
                 'nickname' => '妹子のばあば',
                 'email'=> 'ono@ono.com',
                 'password' => bcrypt('password'),
