f607eb138b2c93b8a64086fea3abe816001e748f
masayuki <masayuki201_201@yahoo.co.jp>
2021-05-18 22:42:49 +0900

    add:VideosTableSeeder.php 動画ID:1コメント追加#348

--- database/seeds/VideosTableSeeder.php
+++ database/seeds/VideosTableSeeder.php
@@ -15,6 +15,7 @@ class VideosTableSeeder extends Seeder
     {
         DB::table('videos')->insert([
             [
+                // 動画ID:1
                 'user_id' =>'1',
                 'url' => 'PkDfrVdCwCs',
                 'target_id' => '1',
