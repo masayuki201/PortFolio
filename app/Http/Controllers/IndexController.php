9e9c32729a6ff62046e44574fe80a331016ecd75
masayuki <masayuki201_201@yahoo.co.jp>
2021-06-07 12:03:33 +0900

    add:IndexController.php  コメント追加#360

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -13,14 +13,10 @@ class IndexController extends Controller
     public function index(Request $request)
     {
         // フォームから検索文字列と選択カテゴリーidを取得
-        // $search_text = $request->search_text;
         $select_target_id = $request->select_target_id;
 
         // クエリビルダで検索を行う(カテゴリーid検索)
         $query = Video::query();
-        // if (isset($search_text)) {
-        //     $query->where('product_name', 'like', '%'.$search_text.'%');
-        // }
         if (isset($select_target_id)) {
             $query->where('target_id', $select_target_id);
         }
@@ -28,24 +24,15 @@ class IndexController extends Controller
         // 並べ替えてページネイトする
         $searches = $query->orderByRaw('id asc')->paginate(50);
 
-
+        // DBに登録されているtargetを取得する(Viewのプルダウンリストで使用)
         $targetGrades = Target::all();
-
-
-        // $users = Video::all();
-        // $videos = Video::all();
 
         // viewに渡すデータ
         $params = [
             'searches' => $searches,
             'targetGrades' => $targetGrades,
-            // 'search_text' => $search_text,
             'select_target_id' => $select_target_id,
-            // 'users' => $users,
-            // 'videos' => $videos,
         ];
-
-
 
     // index.blade.phpを表示させる
         return view('index', $params);
