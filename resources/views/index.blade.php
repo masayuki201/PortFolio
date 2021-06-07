614f736bb63639c9fbc9713e8b982a5a4c3fcbb8
masayuki <masayuki201_201@yahoo.co.jp>
2021-06-07 12:08:00 +0900

    add:Index.blade.php  検索機能追加#361

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,19 +3,53 @@
 @section('title','ちるび/みんなの動画ページ')
 
 @section('content')
+
+{{-- 動画検索 --}}
+<div class="container">
+    <div class="mt-5 mb-5">
+        <h4>おすすめ検索</h4>
+    </div>
+
+{{-- 動画検索フォーム --}}
+<form action="{{ action('IndexController@index') }}" method="get" id="search">
+    {{-- カテゴリの選択 --}}
+    <div class="row">
+        <div class="input-group mt-4 col-md-5 offset-2">
+            <label><h6>おすすめ</h6></label>
+            <select type="number" name="select_target_id" class="ml-2" style=" width:50%; text-align-last:center;">
+                {{-- 画面遷移前に選択したものを初期状態でselectedとする --}}
+                <option value="">全て</option>
+                @foreach ($targetGrades as $key => $targetGrade)
+                @if($targetGrade->id == $select_target_id)
+                    <option value="{{ $targetGrade->id }}" selected>{{ $targetGrade->target_grade }}</option>
+                @else
+                    <option value="{{ $targetGrade->id }}">{{ $targetGrade->target_grade  }}</option>
+                @endif
+                @endforeach
+            </select>
+            {{-- 検索ボタン --}}
+            <span class="input-group-btn">
+                {{ csrf_field() }}
+                <input type="submit" class="btn btn-primary ml-4" value="検索">
+            </span>
+        </div>
+    </div>
+</form>
+</div>
+
+{{-- みんなの動画 --}}
 <div class="container">
     <div class="mt-5 mb-5">
         <h4>みんなの動画</h4>
     </div>
+    {{-- 表示動画の件数 --}}
+    <label class="panel-heading">全 {{ $searches->total() }} 件中 {{ $searches->count() }} 件表示</label>
     <div class="video row mt-5 text-center">
-        @foreach ($users as $key => $user)
-
+        @foreach ($searches as $key => $search)
         @php
-
-        $video=$user;
-
+            $video=$search;
         @endphp
-        {{-- 動画を横に3つずつ表示させる --}}
+            {{-- 動画を横に3つずつ表示させる --}}
             @if($loop->iteration % 3 == 1 && $loop->iteration != 1)
                 </div>
                 <div class="row text-center mt-3">
@@ -23,7 +57,7 @@
                 <div class="col-lg-4 mb-5">
                     <div class="video text-left d-inline-block">
                         {{-- ニックネーム表示 --}}
-                        ＠{{ $video->user['nickname'] }}
+                        ＠{{ $search->user['nickname'] }}
                         <div>
                             {{-- 動画を表示 --}}
                             @if($video)
@@ -34,7 +68,7 @@
                         </div>
                         <p>
                         {{-- 対象学年表示 --}}
-                        おすすめ：{{ $video->target['target_grade'] }}さん
+                        おすすめ：{{ $search->target['target_grade'] }}
                         </p>
                     </div>
                 </div>
