83c7ee795bbb2194471edcf8c1f39e393b39c5b5
masayuki <masayuki201_201@yahoo.co.jp>
2021-06-18 17:53:40 +0900

    update:header.blade.php レスポンシブ化修正#380

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,34 +1,26 @@
 <header>
-    <nav class="navbar navbar-expand-sm navbar-dark" style="background-color: rgb(255, 255, 255)">
-        {{-- サイトロゴ --}}
+    {{-- サイトロゴ --}}
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgb(255, 255, 255)">
         <a class="navbar-brand text-dark" href="/"><h6>Children's<br>Videos</h6></a>
-        <a class="navbar-brand text-dark" href="/"><h1>ちるび</h1></a>
-
-        {{-- 1段目右寄せメニュー ログインしていない場合 新規登録・ログインボタンを表示する--}}
-        @guest
-        <div class="collapse navbar-collapse flex-column" id="nav-bar">
-            <div class="navbar-nav ml-auto">
-                <div class="nav-item">
-                    {!! link_to_route('signup', '新規登録', [], ['class' => 'btn btn-green col-auto']) !!}
-                    {!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-violet col-auto']) !!}
-                </div>
-            </div>
-        </div>
-        @endguest
-
-        {{-- 1段目右寄せメニュー ログインしている場合 ニックネームを表示する--}}
-        @auth
-        <div class="site-description ml-auto">
-            {{Auth::user()->nickname}}さん
-        </div>
-        @endauth
+        <a class="navbar-brand text-dark mr-auto" href="/"><h1>ちるび</h1></a>
     </nav>
+
+    {{-- 1段目右寄せメニュー ログインしている場合 ニックネームを表示する--}}
+    @auth
+    <div class="site-description ml-auto">
+        {{Auth::user()->nickname}}さん
+    </div>
+    @endauth
 
     {{-- 2段左寄せメニュー ログインしていない場合 --}}
     @guest
-    <nav class="navbar navbar-expand-sm navbar-dark" style="background-color: rgb(69, 208, 225)">
-        <div class="collapse navbar-collapse">
-        <ul class="navbar-nav">
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgb(69, 208, 225)">
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#bs-navi" aria-controls="bs-navi" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <nav class="collapse navbar-collapse" id="bs-navi">
+        <ul class="navbar-nav mr-auto">
             <li class="nav-item mx-4">
                 <a class="nav-link text-light faa-parent  animated-hover" href="/pickup">
                     <i class="fa fa-map-pin faa-vertical fa-fw"></i>
@@ -54,14 +46,34 @@
                 </a>
             </li>
         </ul>
-    </div>
+
+    {{-- 2段右寄せメニュー ログインしていない場合 --}}
+        <ul class="navbar-nav">
+            <li class="nav-item mx-4">
+                <a class="nav-link text-light faa-parent  animated-hover" href="/signup">
+                    <i class="fa fa-user faa-flash"></i>
+                    新規登録
+                </a>
+            </li>
+            <li class="nav-item mx-4">
+                <a class="nav-link text-light faa-parent  animated-hover" href="/login">
+                    <i class="fa fa-door-open faa-ring"></i>
+                    ログイン
+                </a>
+            </li>
+        </ul>
+    </nav>
     @endguest
 
     {{-- 2段左寄せメニュー ログインしている時 --}}
     @auth
-    <nav class="navbar navbar-expand-sm navbar-dark" style="background-color: rgb(69, 208, 225)">
-        <div class="collapse navbar-collapse">
-        <ul class="navbar-nav">
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgb(69, 208, 225)">
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#bs-navi" aria-controls="bs-navi" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="bs-navi">
+        <ul class="navbar-nav mr-auto">
             <li class="nav-item mx-4">
                 <a class="nav-link text-light faa-parent  animated-hover" href="/pickup">
                     <i class="fa fa-map-pin faa-vertical fa-fw"></i>
@@ -87,26 +99,23 @@
                 </a>
             </li>
         </ul>
-        </div>
-    @endauth
 
     {{-- 2段右寄せメニュー ログインしている場合 --}}
-        @auth
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
+        <ul class="navbar-nav">
+            <li class="nav-item mx-4">
                 <a class="nav-link text-light faa-parent  animated-hover" href="/user/{{Auth::id()}}">
                     <i class="fa fa-user faa-flash"></i>
                     マイページ
                 </a>
             </li>
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
+            <li class="nav-item mx-4">
                 <a class="nav-link text-light faa-parent  animated-hover" href="/logout">
                     <i class="fa fa-door-open faa-ring"></i>
                     ログアウト
                 </a>
             </li>
         </ul>
-        @endauth
+        </div>
     </nav>
+    @endauth
 </header>
