19a943dd44b165fab4675b2fccac5bb2d6ee8dff
masayuki <masayuki201_201@yahoo.co.jp>
2021-06-25 03:40:17 +0900

    update:header.blade.php レスポンシ修正#408

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -67,7 +67,7 @@
 
     {{-- 2段左寄せメニュー ログインしている時 --}}
     @auth
-    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgb(69, 208, 225)">
+    <nav class="navbar navbar-expand-xl navbar-dark" style="background-color: rgb(69, 208, 225)">
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#bs-navi" aria-controls="bs-navi" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
